00ab58d67c47b69ee05ee2f490adefdbc73732c2
Alexandra Zueva <guseva-1992@yandex.ru>
2022-04-22 22:19:05 +0300

    formatting file 005

--- 005/Program.cs
+++ 005/Program.cs
@@ -1,16 +1,16 @@
 ﻿// 5. Напишите программу, которая принимает на вход два числа и проверяет, 
 // является ли одно число квадратом другого.
 
-int a=3;
-int b=9;
+int a = 3;
+int b = 9;
 
-if(a==b*b)
+if (a == b * b)
 {
     Console.WriteLine("yes");
 }
 else
 {
-    if(b==a*a)
+    if (b == a * a)
     {
         Console.WriteLine("yes");
     }
