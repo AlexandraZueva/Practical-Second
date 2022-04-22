a364f629ff429945fd7de56c01e4788ea9b5b2b2
Alexandra Zueva <guseva-1992@yandex.ru>
2022-04-22 22:18:07 +0300

    add file 005

---
+++ 005/Program.cs
@@ -0,0 +1,21 @@
+﻿// 5. Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+
+int a=3;
+int b=9;
+
+if(a==b*b)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    if(b==a*a)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
